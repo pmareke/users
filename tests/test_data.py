0e510353f96474c4820e67374b707aa9d4d7bf03
Pedro López Mareque <Pedro.lopez.mareque@gmail.com>
2025-05-04 09:04:37 +0200

    feat: refactor TestData class

--- tests/test_data.py
+++ tests/test_data.py
@@ -10,12 +10,8 @@ class TestData:
 
     @staticmethod
     def a_user() -> User:
-        return User(
-            TestData.ANY_USER_ID,
-            TestData.ANY_USER_NAME,
-            TestData.ANY_USER_AGE,
-        )
+        return User(TestData.ANY_USER_ID, TestData.ANY_USER_NAME, TestData.ANY_USER_AGE)
 
     @staticmethod
     def a_payload_from_user(user: User) -> dict:
-        return {"id": str(user.id), "name": user.name, "age": user.age}
+        return {"id": user.id.hex, "name": user.name, "age": user.age}
